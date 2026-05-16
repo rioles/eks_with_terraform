4e95f1d898cd7eca55ce71fe4d1eca46a0e43749
rioles <rioles1992@gmail.com>
2026-05-16 09:26:23 +0100

    terraform fmt

--- aws_eks/variables.tf
+++ aws_eks/variables.tf
@@ -174,7 +174,7 @@ variable "allowed_origins" {
 }
 variable "my_ip" {
   description = "IP publique pour accès SSH"
-  type        = string 
+  type        = string
   default     = "41.79.217.120/32" # Remplace par ton IP réelle (ajoute /32 à la fin)
 }
 
